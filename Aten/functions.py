f4bc5f3ca7094e69ab28206e3040dc84ae6226f0
shauray8 <shauray.209309012@muj.manipal.edu>
2023-02-09 17:06:06 +0530

    Aten with all the functions

---
+++ Aten/functions.py
@@ -0,0 +1,47 @@
+## Do as said in the comments wrt the issue you are working on 
+
+class function:
+  def __init__(self,data):
+    self.data = data
+
+  ## returns the state of the data (do not modify)
+  def __repr__(self):
+    return f"function [{self.data}]"
+
+  ## check if both the numbers are a function(optional) and return self+other.
+  def __add__(self,other):
+    # write your code here and modify the return statement
+    return 
+
+  ## check if both the numbers are a function(optional) and return self*other.
+  def __mul__(self,other):
+    # write your code here and modify the return statement
+    return 
+
+  ## check if both the numbers are a function(optional) and return self-other.
+  def __sub__(self,other):
+    # write your code here and modify the return statement
+    return 
+
+  ## check if both the numbers are a function(optional) and return self * other ** -1.
+  def __truediv__(self,other):
+    # write your code here and modify the return statement
+    return 
+
+  ## check if both the numbers are a function(optional) and return self ** other.
+  def __pow__(self,other):
+    # write your code here and modify the return statement
+    return
+
+  ## check if the number is a function(optional) and return -self.
+  def __neg__(self):
+    # write your code here and modify the return statement
+    return 
+
+
+if __name__ == "__main__":
+  ## Use this to check your code 
+  ## hints : to check if a number is indeed a function use "isinstance"
+  a = function(10)
+  b = function(20)
+  
